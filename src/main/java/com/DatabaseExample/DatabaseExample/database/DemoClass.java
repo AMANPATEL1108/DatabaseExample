5ff8c49fc284276bca8b8db70cd911ba73428709
AMANPATEL <amanpatel8844382@gmail.com>
2024-08-13 17:08:07 +0530

    How To Create a Connection and How To Fetch a Data From DataBase In Java

---
+++ src/main/java/com/DatabaseExample/DatabaseExample/database/DemoClass.java
@@ -0,0 +1,31 @@
+package com.DatabaseExample.DatabaseExample.database;
+import  java.sql.*;
+
+import static java.lang.Class.forName;
+
+public class DemoClass
+{
+    public static void main(String[] args) throws Exception{
+
+        String url="jdbc:mysql://localhost:3306/student";
+        String uname="root";
+        String pass="mysql";
+        String query="select userName from users where userID=3";
+
+        Class.forName("com.mysql.jdbc.Driver");
+
+        Connection con=DriverManager.getConnection(url,uname,pass);
+
+        Statement st=con.createStatement();
+        ResultSet rs=st.executeQuery(query);
+
+        rs.next();
+        String name=rs.getString("userName");
+
+        System.out.println(name);
+
+        st.close();
+        con.close();
+
+    }
+}
---
+++ src/test/java/com/DatabaseExample/DatabaseExample/DatabaseExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DatabaseExample.DatabaseExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
